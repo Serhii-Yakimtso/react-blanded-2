15a48415ac4b76eedca6f4d928b07b293076e582
Serhii-Yakimtso <serafyso@gmail.com>
2024-04-21 12:15:18 +0300

    add first

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,36 @@
-// import { FiSearch } from 'react-icons/fi';
+import { FiSearch } from 'react-icons/fi';
+import style from './Form.module.css';
+import { useState } from 'react';
 
-export const Form = () => {
-  return <h2>Form</h2>;
+export const Form = ({ addTodo }) => {
+  const [text, setText] = useState('');
+
+  const handleChange = event => {
+    // console.log(text);
+    setText(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    // console.log(text);
+    addTodo(text);
+  };
+
+  return (
+    <form className={style.form} onSubmit={handleSubmit}>
+      <button className={style.button} type="submit">
+        <FiSearch size="16px" />
+      </button>
+
+      <input
+        className={style.input}
+        placeholder="What do you want to write?"
+        name="search"
+        required
+        autoFocus
+        value={text}
+        onChange={handleChange}
+      />
+    </form>
+  );
 };
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,5 +1,21 @@
-import { Text } from 'components';
+import { Form, Text } from 'components';
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
 
 export const Todos = () => {
-  return <Text textAlign="center">There are no any todos ...</Text>;
+  const [todo, setTodo] = useState([]);
+
+  const addTodo = text => {
+    console.log(text);
+    setTodo(prewState => {
+      return [...prewState, { text, id: nanoid() }];
+    });
+  };
+
+  return (
+    <>
+      <Form addTodo={addTodo} />
+      <Text textAlign="center">There are no any todos ...</Text>
+    </>
+  );
 };
